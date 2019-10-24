37c79ea65d6997d43b2052910693ad3944000d7d
Jose Carlos <jcarlos.nascimento@outlook.com>
2019-10-24 17:28:19 -0300

    Nova classe de teste unitario

--- src/test/java/br/com/test/PessoaTest.java
+++ src/test/java/br/com/test/PessoaTest.java
@@ -2,21 +2,22 @@ package br.com.test;
 
 import org.jboss.weld.environment.se.Weld;
 import org.jboss.weld.environment.se.WeldContainer;
+import org.junit.Test;
 
 import br.com.base.service.PessoaService;
 
 public class PessoaTest {
-
-	public static void main(String[] args) {
-
+	
+	@Test
+	public void inserirPessoa() {
+		
 		Weld weld = new Weld();
 		WeldContainer weldContainer = weld.initialize();
 		
 		PessoaService ps =  weldContainer.instance().select(PessoaService.class).get();
 		
 		ps.insert(null);
-		
-
 	}
+
 
 }
